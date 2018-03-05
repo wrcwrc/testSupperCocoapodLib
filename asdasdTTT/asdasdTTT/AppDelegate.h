5968e44d1336598d46ae50cb644b8fd5c979c792
hupo <760480765@qq.com>
2018-03-05 11:12:40 +0800

    no message

---
+++ asdasdTTT/asdasdTTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdasdTTT
+//
+//  Created by weirongchi on 2018/3/5.
+//  Copyright © 2018年 weirongchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asdasdTTT/asdasdTTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asdasdTTT
+//
+//  Created by weirongchi on 2018/3/5.
+//  Copyright © 2018年 weirongchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
